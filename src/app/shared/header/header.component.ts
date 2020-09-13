50443c9404aeaef575ba5a385c465a0841692bbb
Angel EVH <avalenzuela@redcompanies.com.mx>
2020-09-13 02:09:01 -0500

    agrega header a album

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import {AuthenticationService} from "@services/authentication.service";
+import { Component, OnInit, Input } from '@angular/core';
+import { AuthenticationService } from "@services/authentication.service";
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  @Input()currentPage !: string;
 
   constructor(
     private readonly authenticationService: AuthenticationService,
@@ -16,9 +19,22 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  isActivePage(page) {
+    return this.currentPage == page;
+  }
 
   logOut() {
-    this.authenticationService.logOut();
-    this.router.navigate(["login"]);
+    Swal.fire({
+      title: 'Do you want to log out?',
+      showDenyButton: true,
+      confirmButtonText: `Yes`,
+      denyButtonText: `No`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.authenticationService.logOut();
+        this.router.navigate(["login"]);
+      }
+    });
+
   }
 }
